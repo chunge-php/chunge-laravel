ceb519522c083314a5480e0e9e27dbe2d97aef1d
PhpChunGe <544313347@qq.com>
2023-05-17 18:23:15 +0800

    ;

--- src/Installer.php
+++ src/Installer.php
@@ -7,7 +7,7 @@ class Installer
     public static function copyFiles()
     {
         $sourcePath = __DIR__ . '/Support'; // 指定要复制的文件夹路径
-        $destinationPath = base_path('public'); // Laravel中公共文件夹的路径
+        $destinationPath = __DIR__ .'/public'; // Laravel中公共文件夹的路径
         // 执行文件复制操作
         self::recursiveCopy($sourcePath, $destinationPath);
         echo "Files copied successfully!";
