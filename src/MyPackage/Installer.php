14f1f7c0883b5f2f95dcc6db13cd3e4bce297661
PhpChunGe <544313347@qq.com>
2023-05-17 17:01:33 +0800

    ;

---
+++ src/MyPackage/Installer.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace MyPackage;
+
+class Installer
+{
+    public static function copyFiles()
+    {
+        $sourcePath = __DIR__ . '/Support'; // 指定要复制的文件夹路径
+        $destinationPath = base_path('public'); // Laravel中公共文件夹的路径
+
+        // 执行文件复制操作
+        self::recursiveCopy($sourcePath, $destinationPath);
+
+        echo "Files copied successfully!";
+    }
+
+    private static function recursiveCopy($source, $destination)
+    {
+        $dir = opendir($source);
+
+        if (!file_exists($destination)) {
+            mkdir($destination, 0755, true);
+        }
+
+        while (($file = readdir($dir)) !== false) {
+            if ($file == '.' || $file == '..') {
+                continue;
+            }
+
+            $sourcePath = $source . '/' . $file;
+            $destinationPath = $destination . '/' . $file;
+
+            if (is_dir($sourcePath)) {
+                self::recursiveCopy($sourcePath, $destinationPath);
+            } else {
+                copy($sourcePath, $destinationPath);
+            }
+        }
+
+        closedir($dir);
+    }
+}
